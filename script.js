96f7e16d5b36db830c1eae4bc3af37f40067875c
Dipak Khillare <dipakkhillare040@gmail.com>
2026-06-11 00:12:16 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function closePopup(){
-    document.getElementById("popup").style.display = "none";
-}
-
-const darkBtn = document.getElementById("darkBtn");
-
-darkBtn.onclick = () => {
-    document.body.classList.toggle("dark");
-};
